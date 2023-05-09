626ad288fbea05ceb2e1cf230d66dd5f52ce22f2
ArielDevFx <122239381+ArielDevFx@users.noreply.github.com>
2023-05-09 10:42:23 -0500

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <a href="https://www.linkedin.com/in/arieldevfx/" target="_blank">
+        <h1>
+          <i className="fab fa-linkedin"></i>
+        </h1>
+      </a>
+      <a href="https://github.com/ArielDevFx" target="_blank">
+        <h1>
+          <i className="fab fa-github-square"></i>
+        </h1>
+      </a>
+    </footer>
+  );
+};
